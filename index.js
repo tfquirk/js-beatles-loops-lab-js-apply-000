7860c8377cb3167d2127f82e9b384b36040895e5
Timothy Quirk <timothyfquirk@gmail.com>
2018-11-04 00:52:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // add solution here
+
+function theBeatlesPlay(arrayMusicians, arrayInstruments) {
+  let musicianPlays = [];
+  
+  for (let i = 0; i < arrayMusicians.length; i++) {
+    musicianPlays.push(arrayMusicians[i] + ' plays ' + arrayInstruments[i]});
+  }
+  return musicianPlays;
+}
